54037d9d19b269b2c654b5ff41c96cf7db5bcb9d
riverandeye <riverandeye@gmail.com>
2019-08-19 16:15:51 +0900

    Librosa add STFT&Spectrogram :runner:

---
+++ Programming_Languages/Python/Librosa/1_STFT&Spectrogram/Spectrogram.py
@@ -0,0 +1,20 @@
+import os
+import numpy as np
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+
+# read file
+File = '../Sample/mp3/Kissin/Kissin_3.mp3'
+y, sr = librosa.load(File)
+
+D = np.abs(librosa.stft(y))
+print(y.shape)
+print(D.shape)
+
+#draw spectrogram
+librosa.display.specshow(librosa.amplitude_to_db(D, ref=np.max),  y_axis='log', x_axis='time')
+plt.title('Power spectrogram')
+plt.colorbar(format='%+2.0f dB')
+plt.tight_layout()
+plt.show()
