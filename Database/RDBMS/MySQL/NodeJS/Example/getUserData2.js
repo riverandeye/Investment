7f9d7f62fd812227450cdce2f31d685b3694701f
riverandeye <riverandeye@gmail.com>
2019-09-25 16:38:32 +0900

    update(MySQL-NodeJS/Example) : fix better code

--- Database/RDBMS/MySQL/NodeJS/Example/getUserData2.js
+++ Database/RDBMS/MySQL/NodeJS/Example/getUserData2.js
@@ -1,33 +1,7 @@
 const db = require('../modules/db2');
 
 async function getData(){
-    const data = await db.query('SELECT * FROM `Card`');
+    const data = await db.promise().query('SELECT * FROM `Card`');
     console.log(data);
 }
 getData();
-
-// async function getConnection(){
-//     return new Promise(
-//         (resolve, reject)=>{
-//             db.getConnection(
-//                 (err, connection)=>{
-//                     !err ? resolve(connection) : reject(err);
-//                 }
-//             )
-//         }
-//     )
-// }
-
-// async function getData(){
-//     const connection = await getConnection();
-
-//     const [rows, fields] = await connection.promise().execute(
-//         'SELECT * FROM `Card`'
-//     )
-
-//     console.log(rows);
-//     connection.release();
-// }
-
-// getData();
-
