0e3318e9a4549a659c92cc51b6be859f9ad326b1
khkang <khkang@promedius.ai>
2021-10-27 14:54:15 +0900

    elixir 2장

---
+++ Programming_Languages/elixir/elixir-in-action/ch2/geometry.ex
@@ -0,0 +1,5 @@
+defmodule Geometry do
+  def area(a), do: area(a, a)
+  # private function
+  defp area(a, b), do: a * b
+end
---
+++ Programming_Languages/elixir/elixir-in-action/ch2/import.ex
@@ -0,0 +1,8 @@
+defmodule MyModule do
+  import IO
+  import Geometry
+
+  def my_function do
+    Geometry.area(10)
+  end
+end
